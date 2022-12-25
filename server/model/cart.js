f697388a9d66c151f19115b386e166c8168883a2
sadiq-b <babalolasadiq@yahoo.com>
2022-12-25 21:51:12 +0100

    created a cart schema

---
+++ server/model/cart.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const cartSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  items: [{
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+      required: true
+    },
+    quantity: {
+      type: Number,
+      required: true
+    }
+  }],
+  total: {
+    type: Number,
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['active', 'completed'],
+    default: 'active'
+  }
+});
+
+module.exports = mongoose.model('Cart', cartSchema);
